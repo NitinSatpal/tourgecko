72486e4113b4bb826274a71b8a3a91f193dc9755
Nitin Satpal <nitinsatpal@gmail.com>
2016-09-22 14:00:56 +0530

    enable multi-core in nodejs

--- config/lib/app.js
+++ config/lib/app.js
@@ -7,6 +7,7 @@ var config = require('../config'),
   mongoose = require('./mongoose'),
   express = require('./express'),
   chalk = require('chalk'),
+  cluster = require('cluster'),
   seed = require('./seed');
 
 function seedDB() {
@@ -32,26 +33,43 @@ module.exports.start = function start(callback) {
   var _this = this;
 
   _this.init(function (app, db, config) {
+    if (cluster.isMaster) {
+      var numWorkers = require('os').cpus().length;
 
-    // Start the app by listening on <port> at <host>
-    app.listen(config.port, config.host, function () {
-      // Create server URL
-      var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
-      // Logging initialization
-      console.log('--');
-      console.log(chalk.green(config.app.title));
-      console.log();
-      console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
-      console.log(chalk.green('Server:          ' + server));
-      console.log(chalk.green('Database:        ' + config.db.uri));
-      console.log(chalk.green('App version:     ' + config.meanjs.version));
-      if (config.meanjs['meanjs-version'])
-        console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
-      console.log('--');
+      console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-      if (callback) callback(app, db, config);
-    });
+      for (var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+      }
+      cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+      });
 
+      cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+      });
+    } else {
+      // Start the app by listening on <port> at <host>
+      app.listen(config.port, config.host, function () {
+        // Create server URL
+        var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
+        // Logging initialization
+        console.log('--');
+        console.log(chalk.green(config.app.title));
+        console.log();
+        console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
+        console.log(chalk.green('Server:          ' + server));
+        console.log(chalk.green('Database:        ' + config.db.uri));
+        console.log(chalk.green('App version:     ' + config.meanjs.version));
+        if (config.meanjs['meanjs-version'])
+          console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
+        console.log('--');
+
+        if (callback) callback(app, db, config);
+      });
+    }
   });
 
 };
