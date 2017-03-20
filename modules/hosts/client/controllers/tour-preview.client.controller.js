7bda2db60f06e26666c70f937a49fe37e4a68d41
Nitin Satpal <nitinsatpal@gmail.com>
2017-03-20 21:44:36 +0530

    minor changes

--- modules/hosts/client/controllers/tour-preview.client.controller.js
+++ modules/hosts/client/controllers/tour-preview.client.controller.js
@@ -32,7 +32,6 @@
         vm.productDetails = response[0];
         vm.companyDetails = response[0].hostCompany;
         vm.productImageURLs = response[0].productPictureURLs;
-        $('.slider').slider({full_width: true});
         $('#tourgeckoBody').removeClass('disableBodyWithoutPosition');
         $('#previewScreen').removeClass('waitCursor');
         vm.showLoaderForPreview = false;
