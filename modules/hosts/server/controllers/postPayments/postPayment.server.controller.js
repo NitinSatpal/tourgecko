cadd68c2aec34dd5a368455447d21c015f6ecdd0
Nitin Satpal <nitinsatpal@gmail.com>
2017-07-04 21:24:25 +0530

    making instamojo mode to test

--- modules/hosts/server/controllers/postPayments/postPayment.server.controller.js
+++ modules/hosts/server/controllers/postPayments/postPayment.server.controller.js
@@ -22,7 +22,7 @@ var Insta = require('instamojo-nodejs');
 Insta.setKeys(config.paymentGateWayInstamojo.instamojoKey, config.paymentGateWayInstamojo.instamojoSecret);
 
 // This line will be removed later. Setting sandbox mode for now
-//Insta.isSandboxMode(true);
+Insta.isSandboxMode(true);
 
 exports.postPaymentEventsAndProcess = function (req, res) {
   Booking.findOne({paymentRequestId: req.body.paymentRequestId, isPaymentDone: false}).populate('product').populate('productSession').exec(function (err, booking) {
