bf303e4f1e35f21c52cd0148d52670b5bf0900bb
Nitin Satpal <nitinsatpal@gmail.com>
2017-08-17 10:50:52 +0530

    changes

--- modules/core/client/controllers/abstracthome.client.controller.js
+++ modules/core/client/controllers/abstracthome.client.controller.js
@@ -9,8 +9,6 @@
 
   function AbstractHomeController($scope, $rootScope, $state, $http, $location, $stateParams, $window) {
     var vm = this;
-    console.log('will it help me seriously ' + $window.location.href);
-    console.log('did it help ' + window.location.href);
     var hostURL = $location.host();
     var tourHostToursite = hostURL.split('.')[0];
     if (tourHostToursite !== 'tourgecko' && tourHostToursite !== 'test' && tourHostToursite !== 'localhost' && tourHostToursite !== 'www') {
@@ -24,16 +22,18 @@
         vm.error = response.message;
       });
     } else {
+      // using nginx we are redirecting from http to https
+      // using nginx only we were redirecting from www to non www, but in that case, subdomains were poinint to main
+      // domain. Hence this is the workaround for forwarding all traffic from www to non www.
+      // going forward we need to find how to achieve it using nginx
       if (tourHostToursite == 'www') {
         var targetURL = '';
         for (var index = 1; index < hostURL.split('.').length; index++) {
           targetURL = targetURL + hostURL.split('.')[index];
-          if (index != hostURL.split('.').length)
+          if (index < hostURL.split('.').length - 1)
             targetURL = targetURL + '.';
         }
-        console.log ('akakakakakaakak ' + targetURL);
-        //$window.location.href = $window.location.protocol + '//' + response.toursite + '.' + $window.location.host;
-        //$window.location.href = 'https://tourgecko.com';
+        $window.location.href = $window.location.protocol + '//' + targetURL;
       } else {
         $state.go('abstractHome.home');
       }
