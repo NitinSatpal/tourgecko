17450654df1552ac3b74858629ee8646516ea152
Nitin Satpal <nitinsatpal@gmail.com>
2017-08-17 10:34:54 +0530

    changes

--- modules/core/client/controllers/abstracthome.client.controller.js
+++ modules/core/client/controllers/abstracthome.client.controller.js
@@ -13,7 +13,7 @@
     console.log('did it help ' + window.location.href);
     var hostURL = $location.host();
     var tourHostToursite = hostURL.split('.')[0];
-    if (tourHostToursite !== 'tourgecko' && tourHostToursite !== 'test' && tourHostToursite !== 'localhost') {
+    if (tourHostToursite !== 'tourgecko' && tourHostToursite !== 'test' && tourHostToursite !== 'localhost' && tourHostToursite !== 'www') {
       $http.get('/api/host/toursite', { params: { 'toursite': tourHostToursite } }).success(function (response) {
         if (response === null || response === '' || response === undefined) {
           $location.path('/not-found');
@@ -24,6 +24,11 @@
         vm.error = response.message;
       });
     } else {
+      if (tourHostToursite == 'www') {
+        console.log ('akakakakakaakak ' + hostURL.split('.'));
+        //$window.location.href = $window.location.protocol + '//' + response.toursite + '.' + $window.location.host;
+        $window.location.href = 'https://tourgecko.com';
+      }
       $state.go('abstractHome.home');
     }
   }
